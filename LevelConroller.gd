0baf99e678792ce5ba0c0931f63c67691356e0ee
Roxanne T <roxanne.e.taylor@gmail.com>
2022-11-21 13:39:26 -0800

    optimized achievements

--- LevelConroller.gd
+++ LevelConroller.gd
@@ -9,17 +9,19 @@ var sleeping: bool = false
 
 const default_multiplier = 10
 const CAT = "PlayerCat"
-var ach_bed: bool = false
-const bed_multiplier = 0.2
-const BED_NAME = "Slept on Bed"
-var ach_ball: bool = false
-var ball_modifier = 0.2
-const BALL_NAME = "Played with Ball"
 
-var achieved = {
-	"BED_NAME" : false,
-	"BALL_NAME" : false
-}
+class Achievement:
+	var name: String
+	var multiplier: int = 0
+	var active: bool = false
+	var achieved: bool = false
+	
+	func _init(name, multiplier):
+		self.name = name
+		self.multiplier = multiplier
+
+var slept_on_bed: Achievement
+var played_with_ball: Achievement
 
 var config
 const FILE_NAME = "user://configs.cfg"
@@ -27,6 +29,8 @@ const SECTION_ACHIEVEMENTS = "achievements"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	slept_on_bed = Achievement.new("slept_on_bed", 0.2)
+	played_with_ball = Achievement.new("played_with_ball", 0.2)
 	score = 0;
 	$Bars/Hunger.frame = 0
 	$Bars/Hunger.play()
@@ -34,10 +38,10 @@ func _ready():
 	var err = config.load(FILE_NAME)
 	if err != OK:
 		print(err)
-	achieved[BALL_NAME] = config.get_value(SECTION_ACHIEVEMENTS, BALL_NAME, false)
-	achieved[BED_NAME] = config.get_value(SECTION_ACHIEVEMENTS, BED_NAME, false)
-	print("Ball:",config.get_value(SECTION_ACHIEVEMENTS, BALL_NAME, false), 
-		"Bed:",config.get_value(SECTION_ACHIEVEMENTS, BED_NAME, false))
+	played_with_ball.achieved = config.get_value(SECTION_ACHIEVEMENTS, played_with_ball.name, false)
+	slept_on_bed.achieved = config.get_value(SECTION_ACHIEVEMENTS, slept_on_bed.name, false)
+	print("Ball:",config.get_value(SECTION_ACHIEVEMENTS, played_with_ball.name, false), 
+		"Bed:",config.get_value(SECTION_ACHIEVEMENTS, slept_on_bed.name, false))
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -46,7 +50,7 @@ func _ready():
 
 func _process(_delta):
 	if sleeping:
-		var modifiers = 1 + (bed_multiplier if ach_bed else 0) + (ball_modifier if ach_ball else 0)
+		var modifiers = 1 + (slept_on_bed.multiplier if slept_on_bed.active else 0) + (played_with_ball.multiplier if played_with_ball.active else 0)
 		score = score + default_multiplier * modifiers
 		change_debug_label($DebugLabel, score)
 		change_debug_label($Modifier, modifiers)
@@ -72,21 +76,21 @@ func _on_PlayerCat_cat_sleep():
 	else:
 		sleeping = true
 		$Bars.sleep()
-		if ach_bed:
-			achieved[BED_NAME] = true
-		if ach_ball:
-			achieved[BALL_NAME] = true
-		config.set_value(SECTION_ACHIEVEMENTS, BED_NAME, achieved[BED_NAME])
-		config.set_value(SECTION_ACHIEVEMENTS, BALL_NAME, achieved[BALL_NAME])
+		if slept_on_bed.active:
+			slept_on_bed.achieved = true
+		if played_with_ball.active:
+			played_with_ball.achieved = true
+		config.set_value(SECTION_ACHIEVEMENTS, slept_on_bed.name, slept_on_bed.achieved)
+		config.set_value(SECTION_ACHIEVEMENTS, played_with_ball.name, played_with_ball.achieved)
 		config.save(FILE_NAME)
-		print("Ball:",config.get_value(SECTION_ACHIEVEMENTS, BALL_NAME, false), 
-			"Bed:",config.get_value(SECTION_ACHIEVEMENTS, BED_NAME, false))
+		print("Ball:",config.get_value(SECTION_ACHIEVEMENTS, played_with_ball.name, false), 
+			"Bed:",config.get_value(SECTION_ACHIEVEMENTS, slept_on_bed.name, false))
 		
 
 func _on_PlayerCat_cat_wake():
 	sleeping = false
 	$Bars.wake()
-	ach_ball = false
+	played_with_ball.active = false
 
 func _on_Energy_animation_finished():
 	if sleeping:
@@ -100,16 +104,16 @@ func _on_Hunger_animation_finished():
 func _on_AchBed_body_entered(body):
 	print(body.name, " is on the bed")
 	if body.name == CAT:
-		ach_bed = true
+		slept_on_bed.active = true
 
 
 func _on_AchBed_body_exited(body):
 	print(body.name, " is off the bed")
 	if body.name == CAT:
-		ach_bed = false
+		slept_on_bed.active = false
 
 
 func _on_Ball_body_entered(body):
 	if body.name == CAT:
 		$Ball/BallAudioStreamPlayer.play()
-		ach_ball = true
+		played_with_ball.active = true
